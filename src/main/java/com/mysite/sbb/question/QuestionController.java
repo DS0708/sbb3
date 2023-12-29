9b3523c6c6933db2ba9cb011e6de0b9e72d7475e
DS0708 <tlsehdtjd070@naver.com>
2023-12-30 00:57:19 +0900

    2-07 질문 목록과 템플릿

---
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -0,0 +1,25 @@
+package com.mysite.sbb.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+
+    private final QuestionRepository questionRepository;
+
+    @GetMapping("/question/list")
+    public String list(Model model){
+//        Model 객체는 자바 클래스와 템플릿 간의 연결고리 역할을 한다. Model 객체에 값을 담아두면 템플릿에서 그 값을 사용할 수 있다.
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList",questionList);
+
+        return "question_list";
+    }
+}
