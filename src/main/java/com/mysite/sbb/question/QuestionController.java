cf12742a6f6d98f12a9f3e8a053138b9d0c3f67d
DS0708 <tlsehdtjd070@naver.com>
2024-01-13 22:59:30 +0900

    03 - 02 페이징 기능 추가하기

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -3,6 +3,7 @@ package com.mysite.sbb.question;
 import com.mysite.sbb.answer.AnswerForm;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,10 +19,15 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping("/list")
-    public String list(Model model){
+    public String list(Model model,
+                       @RequestParam(value = "page", defaultValue = "0") int page){
 //        Model 객체는 자바 클래스와 템플릿 간의 연결고리 역할을 한다. Model 객체에 값을 담아두면 템플릿에서 그 값을 사용할 수 있다.
-        List<Question> questionList = this.questionService.getList();
-        model.addAttribute("questionList",questionList);
+//        List<Question> questionList = this.questionService.getList();
+//        model.addAttribute("questionList",questionList);
+
+        //Paging
+        Page<Question> paging = this.questionService.getList(page);
+        model.addAttribute("paging", paging);
 
         return "question_list";
     }
--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -2,9 +2,14 @@ package com.mysite.sbb.question;
 
 import com.mysite.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,6 +37,18 @@ public class QuestionService {
                 .content(content)
                 .createDate(LocalDateTime.now())
                 .build());
+    }
+
+    public Page<Question> getList(int page){
+        // 역순정렬
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        // desc는 내림차순을 의미하고, asc는 오름차순을 의미한다.
+
+        Pageable pageable = PageRequest.of(page,10, Sort.by(sorts));
+//      page는 조회할 페이지의 번호이고 10은 한 페이지에 보여 줄 게시물의 개수를 의미
+
+        return this.questionRepository.findAll(pageable);
     }
 
 //    public void insert_test() {
